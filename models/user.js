30cab613f3dac7d53f2e2631d27eddc3d629f738
lissaluck <lissaluck@gmail.com>
2017-03-13 17:18:40 -0600

    Add assignment schema

--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
-var uSchema = new Schema({
+var userSchema = new Schema({
     username: {
         type: String,
         required: true,
