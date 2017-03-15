f9b8747561718350776c33a4e17c6ad7a50c96a2
Tyson Nelson <13tyson13@gmail.com>
2017-03-15 13:49:08 -0600

    added input fields to the login and sign up folders as well as auth.js.

--- public/components/assignments/assignments.js
+++ public/components/assignments/assignments.js
@@ -1,7 +1,31 @@
 angular.module("myApp") 
 
-.controller("AssignmentsController", ["$scope", function($scope){
+.service("AssignmentService", ["$http", function ($http) {
+    this.getAssignments = function () {
+        return $http.get("/api/student").then(function (response) {
+            return response.data;
+        }, function (response) {
+            alert("Error " + response.status + ": " + response.statusText);
+        });
+    };
 
-    $scope.name = "Assignments";
-    
+    this.saveAssignment = function (assignment) {
+        return $http.post("/api/student", assignment).then(function (response) {
+            return response.data;
+        }, function (response) {
+            alert("Error " + response.status + ": " + response.statusText);
+        });
+    };
 }])
+
+.controller("AssignmentsController", ["$scope", "$http", "AssignmentService", function ($scope, $http, AssignmentService) {
+    $scope.assignment = {};
+    $scope.assignments = [];
+
+//    (function getAssignments() {
+//        AssignmentService.getAssignments().then(function (assignments) {
+//            $scope.assignments = assignments;
+//        });
+//    })();
+}]);
+    
