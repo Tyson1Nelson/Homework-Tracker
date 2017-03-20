ae453e2a882efceb9a19dd8bc6ed6f42f025b1af
lissaluck <lissaluck@gmail.com>
2017-03-20 14:55:42 -0600

    Add assignment creation

--- public/components/assignments/assignmentService.js
+++ public/components/assignments/assignmentService.js
@@ -9,12 +9,4 @@ angular.module("myApp")
             alert("Error " + response.status + ": " + response.statusText);
         });
     };
-
-    this.saveAssignment = function (assignment) {
-        return $http.post("/api/children", assignment).then(function (response) {
-            return response.data;
-        }, function (response) {
-            alert("Error " + response.status + ": " + response.statusText);
-        });
-    };
-}])
+}]);
