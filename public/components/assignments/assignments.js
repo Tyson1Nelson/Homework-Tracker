8a87d54587415dfedeaf1a1d060b768a23c72098
Tyson Nelson <13tyson13@gmail.com>
2017-03-20 13:23:05 -0600

    added a assignments service

--- public/components/assignments/assignments.js
+++ public/components/assignments/assignments.js
@@ -1,31 +1,16 @@
-angular.module("myApp") 
-
-.service("AssignmentService", ["$http", function ($http) {
-    this.getAssignments = function () {
-        return $http.get("/api/student").then(function (response) {
-            return response.data;
-        }, function (response) {
-            alert("Error " + response.status + ": " + response.statusText);
-        });
-    };
-
-    this.saveAssignment = function (assignment) {
-        return $http.post("/api/student", assignment).then(function (response) {
-            return response.data;
-        }, function (response) {
-            alert("Error " + response.status + ": " + response.statusText);
-        });
-    };
-}])
+angular.module("myApp")
 
 .controller("AssignmentsController", ["$scope", "$http", "AssignmentService", function ($scope, $http, AssignmentService) {
-    $scope.assignment = {};
-    $scope.assignments = ["hello", "goodbye", "see you around"];
-
-//    (function getAssignments() {
-//        AssignmentService.getAssignments().then(function (assignments) {
-//            $scope.assignments = assignments;
-//        });
-//    })();
-}]);
+    //    $scope.assignment = {};
+    $scope.assignments = [];
     
+    AssignmentService.getAssignments().then(function (response) {
+        $scope.assignments = response;
+    });
+
+    //    (function getAssignments() {
+    //        AssignmentService.getAssignments().then(function (assignments) {
+    //            $scope.assignments = assignments;
+    //        });
+    //    })();
+}]);
