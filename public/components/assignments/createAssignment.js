8577f48b832c5f99c80f4d1512b4d97574408386
lissaluck <lissaluck@gmail.com>
2017-03-20 22:31:02 -0600

    Add notes to assignments folder files

--- public/components/assignments/createAssignment.js
+++ public/components/assignments/createAssignment.js
@@ -1,3 +1,5 @@
+// controllers to be added to assignments.js
+
 angular.module("myApp")
 
 .controller("CreateAssignmentsController", ["$scope", "AssignmentService", function ($scope, AssignmentService) {
